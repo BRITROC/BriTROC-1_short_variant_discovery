b2f9adae44915da15926afedaa7f4764de38fd0d
TBradley27 <thomas.bradley435@gmail.com>
2021-11-20 19:46:07 +0000

    generate metadata for patients with tumour samples of both types

--- workflow/rules/generate_metadata.smk
+++ workflow/rules/generate_metadata.smk
@@ -12,6 +12,12 @@ rule get_all_tumour_metadata:
 		somatic_metadata='config/all_tumour_metadata.tsv'
 	script: '../scripts/generate_metadata/database_joins.R'
 
+# generate metadata for all patients with at least one archival and relapse sample but not necessarily a germline sample
+rule get_tumour_metadata_one_each_type:
+	output:
+		somatic_metadata='config/tumour_metadata_with_one_of_both_types.tsv'
+	script: '../scripts/generate_metadata/database_joins_one_tumour_sample_of_each_type.R'
+
 # generate germline and somatic metadata for the exmaination of all genes found in both panel 6 and panel 28 - ensuring there is at least one sample of each type
 rule get_matched_metadata:
 	output: 
---
+++ workflow/scripts/generate_metadata/database_joins_one_tumour_sample_of_each_type.R
@@ -0,0 +1,41 @@
+# use the database to identify germline samples which were sequenced using amplicon panel 28
+
+library(tidyverse)
+library(DBI)
+library(RPostgres)
+
+readRenviron('~/.Renviron')
+
+britroc_con <- dbConnect(RPostgres::Postgres(),
+                 dbname='britroc1',
+                 host='jblab-db.cri.camres.org',
+                 port = 5432,
+                 user = Sys.getenv('jblab_db_username'),
+                 password = Sys.getenv('jblab_db_password')
+)
+
+# example usage
+samples = RPostgres::dbReadTable(britroc_con, 'sample')
+libraries = RPostgres::dbReadTable(britroc_con, 'slx_library')
+experiments = RPostgres::dbReadTable(britroc_con, 'experiment')
+run_slx = RPostgres::dbReadTable(britroc_con, 'run_slx')
+
+somatic_metadata = dplyr::inner_join(libraries, samples, by=c('fk_sample'='name')) %>%
+	dplyr::inner_join(experiments, by=c('fk_experiment'='name')) %>%
+	dplyr::inner_join(run_slx, by='fk_slx') %>%
+	dplyr::filter(type %in% c('archival','relapse')) %>%
+	dplyr::filter(fk_amplicon_panel %in% c(28)) %>%
+	dplyr::arrange(fk_britroc_number)
+
+# only include patients that have at least one tumour sample of each type
+print(somatic_metadata %>% tibble::as_tibble())
+
+patients_with_tumour_samples_of_both_types = 
+	somatic_metadata %>% dplyr::group_by(fk_britroc_number,type) %>% dplyr::summarise(n=dplyr::n()) %>% 
+	dplyr::group_by(fk_britroc_number) %>% dplyr::summarise(n=dplyr::n()) %>% dplyr::filter(n==2) %>% dplyr::pull(fk_britroc_number)
+
+somatic_metadata = somatic_metadata %>% dplyr::filter(fk_britroc_number %in% patients_with_tumour_samples_of_both_types)
+
+somatic_metadata = somatic_metadata %>% mutate(flowcell=stringr::str_extract(string=fk_run, pattern='[-A-Z0-9]+$'))
+
+write.table(somatic_metadata, snakemake@output[['somatic_metadata']], row.names=FALSE, quote=FALSE, sep='\t')
