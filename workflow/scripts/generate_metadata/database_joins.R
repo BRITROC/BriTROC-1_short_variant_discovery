0b2d3059022d677bce7a770665910fb1c68853c2
TBradley27 <thomas.bradley435@gmail.com>
2022-01-23 21:30:42 +0000

    alter logic for generation of all-tumour metadata sheet

--- workflow/scripts/generate_metadata/database_joins.R
+++ workflow/scripts/generate_metadata/database_joins.R
@@ -29,4 +29,11 @@ somatic_metadata = dplyr::inner_join(libraries, samples, by=c('fk_sample'='name'
 
 somatic_metadata = somatic_metadata %>% mutate(flowcell=stringr::str_extract(string=fk_run, pattern='[-A-Z0-9]+$'))
 
+tp53_somatic_metadata = readr::read_tsv(snakemake@input[['somatic_tp53_metadata']])
+
+patients_with_samples_sequenced_for_tp53 = tp53_somatic_metadata %>% dplyr::pull(fk_britroc_number)
+
+# filter so we only examine non-TP53 genes for patient which had TP53 sequencing for at least one sample
+somatic_metadata = somatic_metadata %>% dplyr::filter(fk_britroc_number %in% patients_with_samples_sequenced_for_tp53)
+
 write.table(somatic_metadata, snakemake@output[['somatic_metadata']], row.names=FALSE, quote=FALSE, sep='\t')
